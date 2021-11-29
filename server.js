80b5f3b079bbe972af924bc5b961db893ceef5ff
trujilml <mickeytrj@gmail.com>
2021-11-28 19:09:55 -0600

    starting functions needed for project 11

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
-
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
+const app = express();
 const PORT = process.env.PORT || 3001; 
+
+app.use(express.json());
+
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${port}!`);
